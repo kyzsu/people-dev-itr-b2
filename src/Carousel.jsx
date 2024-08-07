005b07537952dcc5abde37a68d2899739306ad3d
Immanuel W <l0calh34rtz@gmail.com>
2024-08-07 10:46:50 +0700

    feat: implement carousel class component

--- src/Carousel.jsx
+++ src/Carousel.jsx
@@ -1,28 +1,41 @@
 import { Component } from "react"
 
 class Carousel extends Component {
+  // functional comp => hooks / use-usenya, class comp => state = {}
   state = {
     active: 0,
   }
 
+  // TUJUANNYA UNTUK MENAMPILKAN GAMBAR DEFAULT,
+  // APABILA IMAGES YANG DITERIMA DARI PARENT COMPONENT TIDAK ADA (LINE 17).
   static defaultProps = {
     images: ["http://pets-image.dev-apis.com/pets/none.jpg"],
   }
 
+  // FUNGSI UNTUK MENG-HANDLE CLICK EVENT PADA THUMBNAIL
+  handleIndexClick = (event) => {
+    this.setState({
+      active: +event.target.dataset.index,
+    })
+  }
+
   render() {
-    const { active } = this.state
-    const { images } = this.props
+    const { active } = this.state // asalnya dari komponen ini sendiri
+    const { images } = this.props // asal dari parent component
 
     return (
       <div className="carousel">
         <img src={images[active]} alt="pet" />
         <div className="carousel-smaller">
           {images.map((photo, index) => (
+            // eslint-disable-next-line jsx-a11y/click-events-have-key-events, jsx-a11y/no-noninteractive-element-interactions
             <img
               key={index}
               src={photo}
               className={index === active ? "active" : ""}
               alt="pet thumbnail"
+              onClick={this.handleIndexClick}
+              data-index={index}
             />
           ))}
         </div>
--- src/Details.jsx
+++ src/Details.jsx
@@ -2,6 +2,7 @@ import { useParams } from "react-router-dom"
 import { useQuery } from "@tanstack/react-query"
 
 import fetchPet from "./fetchPet"
+import Carousel from "./Carousel.jsx" // IMPOR CAROUSEL
 
 const Details = () => {
   const params = useParams()
@@ -23,6 +24,7 @@ const Details = () => {
 
   return (
     <div className="details">
+      <Carousel images={pet.images} />
       <div>
         <h1>{pet.name}</h1>
         <h2>{`${pet.animal} - ${pet.breed} | ${pet.city} - ${pet.state}`}</h2>
